d1d94ff47d085824c15932fde98f9d45687d1fa1
TylerAStephens <tylerstephens16@yahoo.com>
2020-07-09 21:47:59 -0400

    JSON.parse temperature

--- app.js
+++ app.js
@@ -6,13 +6,20 @@ const app = express();
 
 app.get("/", function(req, res) {
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Orlando&appid=f00839d78e1254228f05bdad1fa805f2&units=imperial";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Orlando&appid=f00839d78e1254228f05bdad1fa805f2&units=imperial";
 
-  https.get(url, function(response){
+  https.get(url, function(response) {
     console.log(response.statusCode);
-  })
+
+    response.on("data", function(data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      console.log(temp);
+    });
+
+  });
   res.send("Server Running");
-})
+});
 
 
 app.listen(3000, function() {
