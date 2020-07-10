2e2355909f516242345bba3e917893dc5f10f925
TylerAStephens <tylerstephens16@yahoo.com>
2020-07-09 21:35:29 -0400

    Set up local server, packages, and API

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+
+app.get("/", function(req, res) {
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Orlando&appid=f00839d78e1254228f05bdad1fa805f2&units=imperial";
+
+  https.get(url, function(response){
+    console.log(response.statusCode);
+  })
+  res.send("Server Running");
+})
+
+
+app.listen(3000, function() {
+  console.log("Local Server on 3000");
+});
