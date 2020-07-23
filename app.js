75ce680f705c39719168ea0a3cce7e81986dd540
TylerAStephens <tylerstephens16@yahoo.com>
2020-07-23 10:59:44 -0400

    send response to page

--- app.js
+++ app.js
@@ -9,16 +9,18 @@ app.get("/", function(req, res) {
   const url = "https://api.openweathermap.org/data/2.5/weather?q=Orlando&appid=f00839d78e1254228f05bdad1fa805f2&units=imperial";
 
   https.get(url, function(response) {
-    console.log(response.statusCode);
+    console.log("Status Code: " + response.statusCode);
 
     response.on("data", function(data) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
       console.log(temp);
+      console.log(weatherDescription);
+      res.send("<h1>The temperature in Orlando is " + temp + " with " + weatherDescription + ".</h1>");
     });
 
   });
-  res.send("Server Running");
 });
 
 
